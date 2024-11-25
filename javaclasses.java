00811defe57bfaab0a1fa566ec6e6991188c238c
Dismas Ndadila <ndadilladismas@gmail.com>
2024-11-25 19:17:58 +0300

    more java

--- javaclasses.java
+++ javaclasses.java
@@ -1,9 +1,23 @@
 /// My first class in java
 public class javaclasses {
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args ) {
 
         System.out.println("Hello World");
 
     }
+}
+
+class Summation{
+
+    public static void main(String args []){
+
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+
+
+    }
+
 }
